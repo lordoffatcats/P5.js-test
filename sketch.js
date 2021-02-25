a986d720d00caf55507730bc64e4c366eede46d8
lordoffatcats <62671111+lordoffatcats@users.noreply.github.com>
2021-02-25 22:25:53 +0000

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+  let capture;
+  let overlay;
+  let pg;
+  let gif;
+
+
+function setup() {
+    createCanvas(1080, 1920);
+
+    gif = loadGif('assets/Tint.gif')
+
+    capture = createCapture(VIDEO);
+    capture.size(1080, 1920);
+
+  	overlay = loadImage('assets/nei2.png')
+
+  }
+
+  function draw() {
+    background(255);
+
+
+
+    image(capture, 0, 0, 1080, 1920);
+
+    push();
+    tint(255, 127);
+    image(gif, 220, 400);
+    pop();
+
+    image(overlay, 0, 0, 1080, 1920);
+
+  }
